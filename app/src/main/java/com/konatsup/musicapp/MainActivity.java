93be52c1ae9346386146a532a37fe647f90c38ba
konatsup <konatsukomatsu@gmail.com>
2019-03-04 08:05:49 +0900

    Add PlayerFragment and render it

--- app/src/main/java/com/konatsup/musicapp/MainActivity.java
+++ app/src/main/java/com/konatsup/musicapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.konatsup.musicapp;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PlayerFragment fragment = new PlayerFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.container, fragment);
+        transaction.commit();
     }
 }
