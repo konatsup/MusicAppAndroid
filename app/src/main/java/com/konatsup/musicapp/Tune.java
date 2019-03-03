d64c4c14b3178d16f05cb3e1790c9dd600b8a9d8
konatsup <konatsukomatsu@gmail.com>
2019-03-04 08:14:16 +0900

    Add Tune model

---
+++ app/src/main/java/com/konatsup/musicapp/Tune.java
@@ -0,0 +1,43 @@
+package com.konatsup.musicapp;
+
+import io.realm.RealmObject;
+
+public class Tune extends RealmObject {
+    private int id;
+    private String title;
+    private double length;
+    private int like;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public int getLike() {
+        return like;
+    }
+
+    public void setLike(int like) {
+        this.like = like;
+    }
+
+}
