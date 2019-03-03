dd2401c9d9cc9767924fc90efd5186f68e465c9d
konatsup <konatsukomatsu@gmail.com>
2019-03-04 08:40:02 +0900

    Add tune list to MainActivity

--- app/src/main/java/com/konatsup/musicapp/MainActivity.java
+++ app/src/main/java/com/konatsup/musicapp/MainActivity.java
@@ -3,17 +3,59 @@ package com.konatsup.musicapp;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.Calendar;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
+    private Realm realm;
+    private ListView listView;
+    private PlayListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PlayerFragment fragment = new PlayerFragment();
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.container, fragment);
-        transaction.commit();
+        realm = Realm.getDefaultInstance();
+        listView = (ListView) findViewById(R.id.listView);
+
+        add();
+        add();
+        add();
+
+        RealmResults<Tune> result = realm.where(Tune.class).findAll();
+        adapter = new PlayListAdapter(result);
+        listView.setAdapter(adapter);
+
+//        PlayerFragment fragment = new PlayerFragment();
+//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//        transaction.add(R.id.container, fragment);
+//        transaction.commit();
+    }
+
+    public void add(){
+        realm.beginTransaction();
+        Tune tune = realm.createObject(Tune.class);
+        tune.setId(1);
+        tune.setTitle("愛迷エレジー");
+        tune.setArtist("DECO*27");
+        tune.setLength(184.0);
+        tune.setLike(20);
+        realm.commitTransaction();
+
+    }
+
+    public void deleteAll(){
+        final RealmResults<Tune> result = realm.where(Tune.class).findAll();
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                result.deleteAllFromRealm();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/konatsup/musicapp/PlayListAdapter.java
@@ -0,0 +1,80 @@
+package com.konatsup.musicapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import io.realm.OrderedRealmCollection;
+import io.realm.RealmBaseAdapter;
+
+public class PlayListAdapter extends RealmBaseAdapter<Tune> implements ListAdapter {
+
+    public PlayListAdapter(OrderedRealmCollection<Tune> realmResults){
+        super(realmResults);
+    }
+
+    public static class ViewHolder{
+        ImageView imageView;
+        TextView titleTextView;
+        TextView artistTextView;
+        TextView likeTextView;
+
+        public ViewHolder(View view){
+            imageView = (ImageView)view.findViewById(R.id.imageView);
+            titleTextView = (TextView) view.findViewById(R.id.titleTextView);
+            artistTextView = (TextView) view.findViewById(R.id.artistTextView);
+            likeTextView = (TextView) view.findViewById(R.id.likeTextView);
+        }
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        final ViewHolder viewHolder;
+
+        // Viewを再利用している場合は新たにViewを作らない
+        if (convertView == null) {
+            convertView =  LayoutInflater.from(parent.getContext()).inflate(R.layout.playlist, parent,false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        if(adapterData != null){
+            final Tune item = adapterData.get(position);
+            viewHolder.imageView.setImageResource(R.drawable.jacket1);
+            viewHolder.titleTextView.setText(item.getTitle());
+            viewHolder.artistTextView.setText(item.getArtist());
+            viewHolder.likeTextView.setText(String.valueOf(item.getLike()));
+
+            /* ClickListener系はとりあえず外しておく*/
+//            viewHolder.titleTextView.setOnClickListener(new View.OnClickListener(){
+//                @Override
+//                public void onClick(View v){
+//                    if(viewHolder.titleTextView.getText().toString()==""){
+//                        viewHolder.titleTextView.setText(item.getTitle());
+//                    }else {
+//                        viewHolder.titleTextView.setText("");
+//                    }
+//                }
+//            });
+//            viewHolder.contentTextView.setOnClickListener(new View.OnClickListener(){
+//                @Override
+//                public void onClick(View v){
+//                    if(viewHolder.contentTextView.getText().toString()==""){
+//                        viewHolder.contentTextView.setText(item.getContent());
+//                    }else {
+//                        viewHolder.contentTextView.setText("");
+//                    }
+//                }
+//            });
+        }
+        return convertView;
+    }
+
+}
